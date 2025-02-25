6187e9afd9094e43cb74cfe78a3e14117ddfe8c5
Rojyx <rojpogz09@gmail.com>
2025-02-25 18:53:47 +0800

    Add files via upload

--- Newew/src/newew/Main.java
+++ Newew/src/newew/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       NewJFrame frame = new NewJFrame();
+       StudentRegistrationForm frame = new StudentRegistrationForm();
        
        frame.setVisible(true);
     }
