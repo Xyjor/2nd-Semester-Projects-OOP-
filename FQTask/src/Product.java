891036de33b04e9e5bb9bd15d7c2ca5f7ffe8bc4
Rojyx <rojpogz09@gmail.com>
2025-03-06 22:21:09 +0800

    Add files via upload

---
+++ FQTask/src/Product.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Xyjor
+ */
+public class Product {
+    private String Type;
+    private String Name;
+    private double Price;
+
+    public Product(String Type, String Name, double Price) {
+        this.Type = Type;
+        this.Name = Name;
+        this.Price = Price;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double Price) {
+        this.Price = Price;
+    }
+
+    
+    
+    
+}
